08dd6b99c92204d7c1349afb7f7c2597dae47a89
TEAMMYKUNG <natthaphong.team@gmail.com>
2021-12-23 17:58:58 +0700

    var edit

--- Calculation.py
+++ Calculation.py
@@ -1,20 +1,21 @@
 
-# Define (x,y) phase A,B,C
+
+# !Define (x,y) phase A,B,C
 Xa = 0; # phase A
-Ya = 30
-Xb = 20  # phase B
-Yb = 15
-Xc = 0  # phase C
-Yc = 15
-Xp = 0  # กำหนดจุด P
-Yp = 0
-r = 0.25  # radius of cable or GMR
-# กำหนด V
-r_a = 500
+Ya = 15.1
+Xb = 4  # phase B
+Yb = 15.1
+Xc = 4  # phase C
+Yc = 12.6
+Xp = -2.3  # กำหนดจุด P
+Yp = 15.1
+r = 12.825  # radius of cable or GMR
+# !Defind Phase A,B,C
+r_a = 115
 theta_a = 0
-r_b = 500
+r_b = 115
 theta_b = -120
-r_c = 500
+r_c = 115
 theta_c = 120
 
 # ===============================================
@@ -51,11 +52,11 @@ Matrix=np.array([[np.log(2*Ya/r) , np.log(dab_p/dab) , np.log(dac_p/dac)],
 Inv_Matrix = np.linalg.inv(Matrix)
 Matrix_multi =np.matmul(Inv_Matrix, V)
 q_cart=2*np.pi*E0*np.matmul(Inv_Matrix, V)
-(Va,Ia)=cart2pol(np.real(q_cart[0]), np.imag(q_cart[0]))
-(Vb,Ib)=cart2pol(np.real(q_cart[1]), np.imag(q_cart[1]))
-(Vc,Ic)=cart2pol(np.real(q_cart[2]), np.imag(q_cart[2]))
+(Qa,QIa)=cart2pol(np.real(q_cart[0]), np.imag(q_cart[0]))
+(Qb,QIb)=cart2pol(np.real(q_cart[1]), np.imag(q_cart[1]))
+(Qc,QIc)=cart2pol(np.real(q_cart[2]), np.imag(q_cart[2]))
 print("ระยะห่างระหว่างจุด")
 print(f"dab={dab:.3f} , dac={dac:.3f} , dbc={dbc:.3f} , dab_p={dab_p:.3f} , dac_p={dac_p:.3f} , dbc_p={dbc_p:.3f}")
-print("ระยะห่างระหว่างจุด")
 np.set_printoptions(precision=3)
-print(f"Va={Va}∠{Ia},Vb={Vb}∠{Ib},Vc={Vc}∠{Ic}")
+print(f"Qa={Qa}∠{QIa},Qb={Qb}∠{QIb},Qc={Qc}∠{QIc}")
+print(q_cart )
